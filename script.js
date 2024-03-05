3fbd2db3e9d9d9fe7dbfa29795b1502a1ad50680
IlariaGuarnieri <ilaria.guarnieri@gmail.com>
2024-03-05 17:28:16 +0100

    [add] milestone 1

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 // prendo l'id dell'html e lo passo su js
 const output = document.getElementById('output')
+const listaUser = document.getElementById('listaUser')
 
 const user = {
   firstName : 'Wayne Barnett',
   mansione : 'Founder & CEO',
+  info :['Wayne Barnett', 'Founder & CEO' ]
+}
+console.log(user);
+
+for(let chiave in user){
+  console.log(user[chiave]);
+  // stampo
+  listaUser.innerHTML +=`<li> ${chiave} : ${user[chiave]} </li>`
 }
 
-console.log(user);
-
-user.age = 40;
-console.log(user);
-
 // stampo
-output.innerHTML = user.firstName;
-output.innerHTML = user.mansione;
+// output.innerHTML = user.firstName;
+// output.innerHTML = user.mansione;
+
